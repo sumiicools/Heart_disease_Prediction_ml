5575644afb4a69973f9534801492880542972160
sumiicools <86673986+sumiicools@users.noreply.github.com>
2022-12-06 19:49:00 +0530

    Update hd_ml_py.py

--- hd_ml_py.py
+++ hd_ml_py.py
@@ -9,18 +9,6 @@ from streamlit_option_menu import option_menu
 
 heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-
-
-
-# # sidebar for navigation
-# with st.sidebar:
-    
-#     selected = option_menu('Disease Prediction System',
-                          
-#                           ['Heart Disease Prediction'],
-#                           icons=['heart'],
-#                           default_index=0)
-
 # Heart Disease Prediction Page
 if (selected == 'Heart Disease Prediction'):
     
